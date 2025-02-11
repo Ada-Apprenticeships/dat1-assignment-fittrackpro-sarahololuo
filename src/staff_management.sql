b8d9a1b0190e8ac0841a538ced4a586527518a23
sarahololuo <sarah.ololuo@ada.ac.uk>
2025-02-11 16:17:36 +0000

    task 7.2 completed

--- src/staff_management.sql
+++ src/staff_management.sql
@@ -17,4 +17,14 @@ PRAGMA foreign_keys = ON;
 
 
 -- 2. Find trainers with one or more personal training session in the next 30 days
--- TODO: Write a query to find trainers with one or more personal training session in the next 30 days
+-- TODO: Write a query to find trainers with one or more personal training session in the next 30 days,
+
+--SOLUTION
+-- SELECT s.staff_id AS trainer_id, s.first_name || ' ' || s.last_name AS trainer_name, COUNT(pts.session_id) AS session_count
+-- FROM staff s
+-- JOIN personal_training_sessions pts
+-- ON s.staff_id = pts.staff_id
+-- WHERE DATE(pts.session_date) BETWEEN DATE('now') AND DATE('now', '+30 days')
+-- GROUP BY trainer_id, trainer_name
+-- HAVING session_count >= 1
+-- ORDER BY session_count DESC;
