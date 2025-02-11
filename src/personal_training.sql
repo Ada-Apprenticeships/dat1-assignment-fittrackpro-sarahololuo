17d321321666d0e7dc4d9b78aa25fd1c84719943
sarahololuo <sarah.ololuo@ada.ac.uk>
2025-02-11 16:32:45 +0000

    task 8.1 completed

--- src/personal_training.sql
+++ src/personal_training.sql
@@ -3,8 +3,18 @@
 .mode column
 
 -- Enable foreign key support
+PRAGMA foreign_keys = ON;
 
 -- Personal Training Queries
 
 -- 1. List all personal training sessions for a specific trainer
 -- TODO: Write a query to list all personal training sessions for a specific trainer
+-- List all personal training sessions for specific trainer "Ivy Irwin"
+
+-- SOLUTION
+-- SELECT pts.session_id, s.first_name || ' ' || s.last_name AS member_name, pts.session_date, pts.start_time, pts.end_time 
+-- FROM personal_training_sessions pts
+-- JOIN staff s
+-- ON pts.staff_id = s.staff_id
+-- WHERE member_name = 'Ivy Irwin'
+-- ORDER BY session_date;
