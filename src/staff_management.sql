2556d87f901d63ad70ddf5d5a787099655704b05
sarahololuo <sarah.ololuo@ada.ac.uk>
2025-02-11 15:43:23 +0000

    task 7.1 completed

--- src/staff_management.sql
+++ src/staff_management.sql
@@ -3,11 +3,18 @@
 .mode column
 
 -- Enable foreign key support
+PRAGMA foreign_keys = ON;
 
 -- Staff Management Queries
 
 -- 1. List all staff members by role
 -- TODO: Write a query to list all staff members by role
+
+-- SOLUTION
+-- SELECT staff_id, first_name, last_name, position AS role
+-- FROM staff
+-- ORDER BY role;
+
 
 -- 2. Find trainers with one or more personal training session in the next 30 days
 -- TODO: Write a query to find trainers with one or more personal training session in the next 30 days
