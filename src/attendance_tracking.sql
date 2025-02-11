915045cba124b58babf95798df78649f27d99de6
sarahololuo <sarah.ololuo@ada.ac.uk>
2025-02-11 12:23:48 +0000

    attempted 6.3

--- src/attendance_tracking.sql
+++ src/attendance_tracking.sql
@@ -17,19 +17,24 @@ PRAGMA foreign_keys = ON;
 
 -- 2. Retrieve a member's attendance history
 -- TODO: Write a query to retrieve a member's attendance history
---  Get attendance history for member with ID 5
--- attendance table = check in and check out
--- visit_date | check_in_time | check_out_time
 
 --SOLUTION
 -- SELECT DATE(check_in_time) AS visit_date, TIME(check_in_time) AS check_in_time, TIME(check_out_time) AS check_out_time
 -- FROM attendance
 -- WHERE member_id = 5;
 
-
-
 -- 3. Find the busiest day of the week based on gym visits
 -- TODO: Write a query to find the busiest day of the week based on gym visits
+-- Identify the busiest day of the week based on gym visits
+-- day_of_week | visit_count
+
+-- SOLUTION
+-- SELECT 
+--     DATE(check_in_time) AS day_of_week,
+--     COUNT(*) AS visit_count
+-- FROM attendance
+-- GROUP BY day_of_week
+-- ORDER BY visit_count DESC;
 
 -- 4. Calculate the average daily attendance for each location
 -- TODO: Write a query to calculate the average daily attendance for each location
